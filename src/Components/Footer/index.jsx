7a3197c7308566f03959b8060df0be3f24c395d7
Jonathan Brooks <jonbrooks0122@gmail.com>
2023-09-23 11:35:28 -0700

    completed lab 26

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+// import React from 'react'
 
-class Footer extends React.Component {
-  render() {
-    return <footer>&copy; 2018</footer>;
-  }
+const Footer = () => {
+  return (
+    <footer>&copy; 2018</footer>
+  )
 }
 
-export default Footer;
+export default Footer
+
+
+
+
+// import React from 'react';
+
+// class Footer extends React.Component {
+//   render() {
+//     return <footer>&copy; 2018</footer>;
+//   }
+// }
+
+// export default Footer;
