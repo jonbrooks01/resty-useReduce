e4045989c8507a35f7ebb04cc1d0b99613197e9f
Jonathan Brooks <jonbrooks0122@gmail.com>
2023-09-26 21:15:20 -0700

    almost finished with lab

--- src/App.jsx
+++ src/App.jsx
@@ -8,22 +8,43 @@ import Results from "./Components/Results";
 import Form from "./Components/Form";
 
 const App = () => {
+  const [loading, setLoading] = useState(false);
   const [ applicationState, setApplicationState] = useState({
     data: null,
     requestParams: {},
   });
 
-  const callApi = () => {
-    // mock output
-    const newData = {
-      count: 2,
-      results: [
-        { name: "fake thing 1", url: "http://fakethings.com/1" },
-        { name: "fake thing 2", url: "http://fakethings.com/2" },
-      ],
-    };
-    setApplicationState({ ...applicationState, data: newData });
+  const callApi = async (formData) => {
+    setLoading(true)
+
+    try {
+      const response = await fetch(formData.url, {
+        method: formData.method,
+        body: formData.method === "GET" ? null : formData.requestBody,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      
+      const data = await response.json();
+      setApplicationState({data, requestParams: formData});
+    } catch (error) {
+      console.error("Error", error);
+    } finally {
+      setLoading(false);
+    }
   }
+
+    // mock output
+    // const newData = {
+    //   count: 2,
+    //   results: [
+    //     { name: "fake thing 1", url: "http://fakethings.com/1" },
+    //     { name: "fake thing 2", url: "http://fakethings.com/2" },
+    //   ],
+    // };
+    // // console.log({ ...applicationState, data: newData, ...formData })
+    // setApplicationState({ ...applicationState, data: newData, requestParams: formData });
 
   return (
     // <React.Fragment>
@@ -32,7 +53,7 @@ const App = () => {
       <div>Request Method: {applicationState.requestParams.method}</div>
       <div>URL: {applicationState.requestParams.url}</div>
       <Form handleApiCall={callApi} />
-      <Results data={applicationState.data} />
+      <Results loading={loading} data={applicationState.data} />
       <Footer />
     </>
     //  </React.Fragment>
--- src/Components/Form/index.jsx
+++ src/Components/Form/index.jsx
@@ -1,29 +1,109 @@
-// import React from "react";
+import { useState } from "react";
 import "./Form.scss";
 
 const Form = (props) => {
-  function handleSubmit(event) {
+  const [formData, setFormData] = useState({
+    method: "GET",
+    url: "",
+    requestBody: "",
+  });
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleClick = (event) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleMethodChange = (method) => {
+    setFormData({
+      ...formData,
+      method: method,
+    });
+  };
+
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    const formData = {
-      method: "GET",
-      url: "https://pokeapi.co/api/v2/pokemon",
-    };
-    props.handleApiCall(formData);
-  }
+
+    setIsLoading(true);
+
+    try {
+      const response = await fetch(formData.url, {
+        method: formData.method,
+        body: formData.method === "GET" ? null : formData.requestBody,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      const responseData = await response.json();
+
+      props.callApi(responseData);
+    } catch (error) {
+      console.error("Error", error);
+    } finally {
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 1000);
+    }
+  };
   return (
     <>
       <form onSubmit={handleSubmit}>
         <label>
           <span>URL: </span>
-          <input name="url" type="text" />
+          <input
+            name="url"
+            type="text"
+            value={formData.url}
+            onChange={handleClick}
+          />
           <button type="submit">GO!</button>
         </label>
         <label className="methods">
-          <span id="get">GET</span>
-          <span id="post">POST</span>
-          <span id="put">PUT</span>
-          <span id="delete">DELETE</span>
+          <span
+            id="get"
+            className={formData.method === "GET" ? "active" : ""}
+            onClick={() => handleMethodChange("GET")}
+          >
+            GET
+          </span>
+          <span
+            id="post"
+            className={formData.method === "POST" ? "active" : ""}
+            onClick={() => handleMethodChange("POST")}
+          >
+            POST
+          </span>
+          <span
+            id="put"
+            className={formData.method === "PUT" ? "active" : ""}
+            onClick={() => handleMethodChange("PUT")}
+          >
+            PUT
+          </span>
+          <span
+            id="delete"
+            className={formData.method === "DELETE" ? "active" : ""}
+            onClick={() => handleMethodChange("DELETE")}
+          >
+            DELETE
+          </span>
         </label>
+        <label>
+          <span>Request Body (JSON):</span>
+          <textarea
+            name="requestBody"
+            value={formData.requestBody}
+            onChange={handleClick}
+            rows={4}
+          />
+        </label>
+        {/* Display loading status */}
+        {isLoading && <p>Loading...</p>}
       </form>
     </>
   );
